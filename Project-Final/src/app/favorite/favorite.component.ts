b259fc7670d1c3f4427d377284bf264b562a45d9
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-31 01:46:59 -0400

    Resolved Bug

--- Project-Final/src/app/favorite/favorite.component.ts
+++ Project-Final/src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  @Input() isFavorite = false;
+  @Input() isFavorite: boolean;
   @Output() change = new EventEmitter();
 
   constructor() { }
